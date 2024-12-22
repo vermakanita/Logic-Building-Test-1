afc2a72095b17782bcf3ca82eabe78fefceb4f95
KANITA VERMA <vermakanita13@gmail.com>
2024-12-22 23:14:02 +0530

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,15 @@
+package question;
+
+public class Q5 {
+    public static void main(String[] args) {
+        
+    
+    for(int i=1;i<8;i+=2){
+        for(int j=1;j<=i;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+        
+    }
+}
+}
