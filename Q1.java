e50fd68fe13d0f0531e5cd220e68a254aefc1ee8
KANITA VERMA <vermakanita13@gmail.com>
2024-12-24 23:35:51 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,113 @@
+// 54321
+// ****
+// 321
+// **
+// 1
+
+public class Q1 {
+    public static void main(String[] args) {
+    //     for (int i = 1; i <= 5; i++) {
+            
+    //         for (int j = 5; j >= i; j--) {
+                
+    //             if (i % 2 == 0) {
+    //                 System.out.print("*");
+    //             } else {
+    //                 System.out.print(j);
+    //             }
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+
+// int n=5;
+// for (int i=1;i<=n;i++){
+// for( int j=1;j<=i;j++){
+// System.out.print("*");
+// }
+// System.out.println();
+
+
+
+//   for(int i=1;i<=5;i++){
+//     for(int j=i;j<=5;j++){
+//         // for(int k=5;k>=i;k--){
+//     System.out.print("X");
+//     }
+//     System.out.println();
+//   }
+
+
+
+// for(int i=1;i<=5;i++){
+//     for(int j=i;j<=5;j++){
+//     System.out.print(" ");
+//     }
+    
+    // for(int k=1;k<=i;k++){
+    // System.out.print("*");
+    // }
+    
+    // System.out.println();
+    // }
+    // }
+
+    // for(int i=1;i<=5;i++){
+    //     for(int j=5;j>=i;j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=1;k<=i;k++){
+    //         System.out.print("*");
+    //     }
+    //     for(int k=1;k<i;k++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+
+
+    // for(int i=1;i<=5;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int k=5;k>=i;k--){
+    //         System.out.print("*");
+
+    //     }
+    //     for(int p=5;p>i;p--){
+    //         System.out.print("*");
+    //     }System.out.println();
+    // }
+
+
+
+
+    // for(int i=1;i<=5;i++){
+    //     for(int j=5;j>=i;j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     for(int j=5;j>i;j--){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+    
+    
+    
+
+
+    }}
+
+
+    
+
+ 
